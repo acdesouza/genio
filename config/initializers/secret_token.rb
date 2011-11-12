927e2adfbb375eb99917d8effb4af0d6ea27afb0
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2011-11-12 12:29:00 -0200

    Inicio do projeto: adiciona o arquivo de documentação.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartolla::Application.config.secret_token = '7825d095c79fbeaf3c39e75e0f61a1536a86d16a7667b88a46991460def1afc1f8b4ae1809882cfbc089c1e0ab4d4d5f902fa90da30af7484a5ec078f5564636'

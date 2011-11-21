277a07e42c325f7c218ccc3f71b3ca13c1b8d307
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2011-11-21 01:05:56 -0200

    Lista os desejos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def action_title(title)
+    project_name = 'Cartolla'
+
+    content_for :page_title, "#{project_name} | #{title}"
+    content_tag :h1, title
+  end
 end
--- test/functional/wishes_controller_test.rb
+++ test/functional/wishes_controller_test.rb
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 require 'test_helper'
 
 class WishesControllerTest < ActionController::TestCase
@@ -12,4 +13,25 @@ class WishesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:wishes)
   end
 
+  test "should wish list title equals Nossos desejos" do
+    get :list
+    assert_select 'title', "Cartolla | Nossos desejos"
+  end
+
+  test 'should have a wish list' do
+    get :list
+    assert_select 'table#wishes' do
+      assert_select 'header' do
+        assert_select 'th', 2
+      end
+
+      assert_select 'body' do
+        assert_select 'tr', 2
+        assert_select 'tr' do |trs|
+          assert_select trs[0], 'td.item', 'Samsung LCD 55&quot; FullHD'
+          assert_select trs[0], 'td.price', '1000'
+        end
+      end
+    end
+  end
 end

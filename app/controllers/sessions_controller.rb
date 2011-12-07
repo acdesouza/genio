7c26556c7014b4efacd7966ddac01d81026ec8a6
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2011-12-07 01:33:22 -0200

    Adiciona a funcionalidade de logar e deslogar no site.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    flash[:notice] = "You are now signed out!"
+    #flash[:notice] = "You are now signed out!"
     redirect_to root_path
   end
 end

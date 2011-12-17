fd88373455a1247eaea5d0ff2737755fb1640cff
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2011-12-17 21:27:15 -0200

    Aponta a home para a tela de desejos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Cartolla::Application.routes.draw do
-  devise_for :users, :skip => :all
-  root :to => "sessions#new"
-  post "/users/sign_in"    => "sessions#create"
-  delete "/users/sign_out" => "sessions#destroy"
+  #devise_for :users, :skip => :all
+  #root :to => "sessions#new"
+  #post "/users/sign_in"    => "sessions#create"
+  #delete "/users/sign_out" => "sessions#destroy"
 
   resources :products do
     get :autocomplete_product_name, :on => :collection
   end
 
   resources :wishes
-#  root :to => 'wishes#index'
+  root :to => 'wishes#new'
 end

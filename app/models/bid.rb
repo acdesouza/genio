4db56674e3d8233a3b268bee2d8972e34e9eed3b
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2012-02-08 01:00:46 -0200

    Adiciona o relacionamento entre os modelos de oferta e usuário.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,5 @@
 class Bid < ActiveRecord::Base
   validates :price, :due_to, :qtd, :url, :presence => true
+
+  belongs_to :user
 end
--- test/unit/bid_test.rb
+++ test/unit/bid_test.rb
@@ -4,12 +4,14 @@ require 'test_helper'
 class BidTest < ActiveSupport::TestCase
   test 'should create a bid which grants wishes.' do
     saved_bid = Bid.create  :price  => 100, :due_to => Time.now,
-                            :qtd    =>  10, :url    => 'http://submarino.com/produtcts/10?vendor=cartolla'
+                            :qtd    =>  10, :url    => 'http://submarino.com/produtcts/10?vendor=cartolla',
+                            :user   => users(:everton) 
 
     refute_nil saved_bid
     assert_present saved_bid.price
     assert_present saved_bid.due_to
     assert_present saved_bid.qtd
     assert_present saved_bid.url
+    assert_present saved_bid.user
   end
 end

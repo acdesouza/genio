3d9bde1b90dfc72e25d10a5f105a3690eaa481b2
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2012-03-18 20:23:52 -0300

    Cria o arquivo de seeds com os produtos ofertados.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120217154413) do
+ActiveRecord::Schema.define(:version => 20120213051023) do
 
   create_table "bids", :force => true do |t|
     t.decimal  "price"
@@ -22,19 +22,6 @@ ActiveRecord::Schema.define(:version => 20120217154413) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "product_id"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.text     "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "products", :force => true do |t|
